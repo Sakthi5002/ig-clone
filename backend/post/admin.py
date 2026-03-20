ed8822834753b0316d4f39d1b305ee58af5b0b12
Sakthi2005 <sakthiselvamani2005@gmail.com>
2026-03-20 16:50:12 +0530

    models registered in admin for existing models

--- backend/post/admin.py
+++ backend/post/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post, Like, Comment, HashTag
+
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(HashTag)
